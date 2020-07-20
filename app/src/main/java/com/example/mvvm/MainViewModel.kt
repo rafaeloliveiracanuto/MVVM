894378e17d1d17c99ac58fdfb2eb32f745626534
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-20 20:18:30 -0300

    Add MainViewModel

--- app/src/main/java/com/example/mvvm/MainViewModel.kt
+++ app/src/main/java/com/example/mvvm/MainViewModel.kt
@@ -1,7 +1,25 @@
 package com.example.mvvm
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    private var mRepository = PersonRepository()
+
+    private var mTextWelcome = MutableLiveData<String>()
+    var textWelcome = mTextWelcome
+
+    private var mLogin = MutableLiveData<Boolean>()
+    var login = mLogin
+
+    init {
+        mTextWelcome.value = "Hello World!"
+    }
+
+    fun login(login: String) {
+        val result = mRepository.login(login)
+        mLogin.value = result
+    }
 
 }
